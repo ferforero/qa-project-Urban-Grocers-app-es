97791e62a433d88154133f111957a2055c890c95
Fernanda Forero <fernandaforeroc@live.com>
2025-08-01 10:27:24 -0500

    Actualizo URL del servidor Urban Grocers

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE = "https://cnt-1ccd615c-8487-448d-af51-33905048413d.containerhub.tripleten-services.com"
+URL_SERVICE = " https://cnt-fc28cf65-df12-4e78-a10e-9b5ab01e3ca2.containerhub.tripleten-services.com"
 CREATE_USER_PATH ="/api/v1/users"
 CREATE_KITS_PATH ="/api/v1/kits"
 
